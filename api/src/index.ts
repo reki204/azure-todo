a1202da096e4e1b1f520e2edb7bbff8a443a1cce
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-02-09 17:24:40 +0900

    index修正

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,23 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
+import todo from "./todoRouter";
+
 const app = new Hono();
 
-app.get("/api", (c) => c.text("Hello Azure Functions!"));
+// CORSの設定
+app.use(
+  "/api/*",
+  cors({
+    origin: ["http://localhost:5173"],
+    allowMethods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.get("/api", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.route("/api/todos", todo);
 
 export default app;
