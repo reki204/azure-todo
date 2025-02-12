83c8e0a5203151e6b400385715368e263e90abf4
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-02-12 22:19:34 +0900

    リファクタリング

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,7 @@
 import { Hono } from "hono";
-import { cors } from "hono/cors";
 import todo from "./todoRouter";
 
 const app = new Hono();
-
-// CORSの設定
-app.use(
-  "/api/*",
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://delightful-mud-0f3bf9b00.4.azurestaticapps.net",
-    ],
-    allowMethods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
 
 app.get("/api", (c) => {
   return c.text("Hello Hono!");
